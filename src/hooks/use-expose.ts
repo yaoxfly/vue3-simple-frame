547d130b74b74b77842e4fc5e5054f05bb6dd8da
yaoxfly <1315892088@qq.com>
2022-12-27 15:35:37 +0800

    style:调整hooks

--- src/hooks/use-expose.ts
+++ src/hooks/use-expose.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { getCurrentInstance } from 'vue'
-export function useExpose<T = Record<string, any>> (apis: T) {
+export function useExpose<T = Record<string, any>>(apis: T) {
   const instance = getCurrentInstance()
   if (instance) {
     Object.assign(instance.proxy || {}, apis)
