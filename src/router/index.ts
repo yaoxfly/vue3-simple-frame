43acbecbd9a986c8053508d7a5a8962ab5c4a720
yaoxfly <1315892088@qq.com>
2022-12-27 15:36:57 +0800

    feat: 路由配置优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 export default router
