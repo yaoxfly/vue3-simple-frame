eb9518bd04d66d91e02286b57c0a192b8500a8e7
yaoxfly <1315892088@qq.com>
2022-12-27 15:49:29 +0800

    style:调整请求方法hooks

--- src/request/hook.ts
+++ src/request/hook.ts
@@ -1,11 +1,11 @@
 import httpRequest from '@/request/index'
 import qs from 'qs'
-export type Config ={
+export type Config = {
   method?: string,
 }
 
 /* ----------请求相关方法------------- */
-export const request = <T extends Config> (config:T) => {
+export const request = <T extends Config>(config: T) => {
   const { method } = config
   // get请求默认序列化
   if (method === 'get') {
@@ -18,7 +18,7 @@ export const request = <T extends Config> (config:T) => {
  * @author yx
  */
 
-export const formDataAppend = <T>(obj:T) => {
+export const formDataAppend = (obj: any) => {
   const formData = new FormData()
   Object.keys(obj).forEach(key => {
     formData.append(key, obj[key])
@@ -31,9 +31,9 @@ export const formDataAppend = <T>(obj:T) => {
 /** @description query方式传数据,不带file,序列化URL的形式，以&进行拼接,让params方式可传数组、对象。
  * @author yx
  */
-export const paramsSerializer = <T> () => {
+export const paramsSerializer = <T>() => {
   return {
-    paramsSerializer: (params:T) => qs.stringify(params),
+    paramsSerializer: (params: T) => qs.stringify(params),
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
     }
@@ -43,9 +43,9 @@ export const paramsSerializer = <T> () => {
 /** @description formData方式传数据，不带file的
  * @author yx
  */
-export const transformRequest = <T> () => {
+export const transformRequest = <T>() => {
   return {
-    transformRequest: [(data:T) => qs.stringify(data)],
+    transformRequest: [(data: T) => qs.stringify(data)],
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
     }
@@ -59,7 +59,7 @@ export const transformRequest = <T> () => {
  */
 export const multipartFormData = () => {
   return {
-    transformRequest: [function (data:any, headers:any) {
+    transformRequest: [function (data: any, headers: any) {
       delete headers.post['Content-Type']
       return data
     }]
@@ -71,7 +71,7 @@ export const multipartFormData = () => {
 /** @description Blob 转为json  接收的方式为Blob但返回的却是json的数据， 用来处理导入、导出错误信息提示。
  * @author yx
  */
-export const blobToJson = (data:Blob) => {
+export const blobToJson = (data: Blob) => {
   const reader = new FileReader() // 创建读取文件对象
   reader.readAsText(data, 'utf-8') // 设置读取的数据以及返回的数据类型为utf-8
   reader.addEventListener('loadend', function () {
@@ -83,7 +83,7 @@ export const blobToJson = (data:Blob) => {
 /** @description  二进制流文件下载
  * @author yx
  */
-export const download = (data:Blob, name:string) => {
+export const download = (data: Blob, name: string) => {
   const blob = new Blob([data], { type: 'application/octet-stream' })
   const url = URL.createObjectURL(blob)
   const exportLink = document.createElement('a')
