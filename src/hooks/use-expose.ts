ab2149f06cec770d2c7fcb8ddf84e4efc4998f23
yaoxfly <1315892088@qq.com>
2023-01-05 16:58:49 +0800

    update: 配置了自动导入插件，去除导入的语句

--- src/hooks/use-expose.ts
+++ src/hooks/use-expose.ts
@@ -1,5 +1,4 @@
 /* eslint-disable */
-import { getCurrentInstance } from 'vue'
 export function useExpose<T = Record<string, any>>(apis: T) {
   const instance = getCurrentInstance()
   if (instance) {
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,3 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
